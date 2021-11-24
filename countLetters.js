d0f80974be4f485b53d637195aeccf47f91b0192
MustafaSKaya <mustafakya88@hotmail.com>
2021-11-24 23:34:37 +0000

    added countLetter.js and modified assertEqual.js

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let letterCount = {};
+  for (const letter of string.replace(/\s/g, "")) {
+    if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+};
+
+console.log(countLetters("lighthouse in the house"));
