bbe69a44d16d50e837cfb5c621657e28e2b22140
MustafaSKaya <mustafakya88@hotmail.com>
2021-11-26 02:17:59 +0000

    added map.js

---
+++ map.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } else if (i === arr1.length - 1 && arr1[arr1.length - 1] === arr2[arr2.length - 1]) {
+        return true;
+      }
+    }
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
+console.log(map(words, word => word[0]));
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
