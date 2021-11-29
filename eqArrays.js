e17a699cffdf09494ba715234e327da125a2644d
MustafaSKaya <mustafakya88@hotmail.com>
2021-11-29 20:58:26 +0000

    assertEqualArraysTest file added

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,17 @@
 const eqArrays = (arr1, arr2) => {
     if (arr1.length === arr2.length) {
+      if (arr1.length >= 1) {
         for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) {
-                return false;
-            } else if (i === arr1.length - 1 && arr1[arr1.length - 1] === arr2[arr2.length - 1]) {
-                return true;
-            }
+          if (arr1[i] !== arr2[i]) {
+            return false;
+          } else if (i === arr1.length - 1 && arr1[arr1.length - 1] === arr2[arr2.length - 1]) {
+            return true;
+          }
         }
+      } else {
+        return true
+      }
     }
-};
+  };
 
 module.exports = eqArrays;
