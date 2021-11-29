3c4ff586ae53a3712df47456dc0ec60fdbef1199
MustafaSKaya <mustafakya88@hotmail.com>
2021-11-29 17:57:30 +0000

    assertEqual and head is modified and their test files are created

--- head.js
+++ head.js
@@ -1,16 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === undefined && expected === undefined) {
-    console.log(undefined);
-  } else if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
 const head = function(argumentOne, argumentTwo) {
   if (argumentOne.length > 1) {
     return argumentOne[0];
@@ -20,6 +7,4 @@ const head = function(argumentOne, argumentTwo) {
     return argumentOne;
   }
 };
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+ module.exports = head;
